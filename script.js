2b34173ae12196b45c191c9ebda28a215ec91aa1
lily 🪷 <lb.wuy20@manchesterhigh.net>
2026-03-10 21:56:03 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function displayoverlay(name) {
+  document.getElementById(name).style.display="block"
+  document.getElementById(name).style.animation="1s ease fadein"
+}
+
+function removeoverlay(name) {
+  document.getElementById(name).style.animation="1s ease fadeout";
+  setTimeout(function() {
+  document.getElementById(name).style.display="none"
+  }, 900);
+}
+
+const wrappers = document.getElementsByClassName('wrapper');
+const overlayContents = document.getElementsByClassName('overlay-content');
+
+function scalePage() {
+  const scale = window.innerWidth / 625; 
+  for (let i = 0; i < wrappers.length; i++) {
+    wrappers[i].style.transform = `scale(${scale})`;
+    wrappers[i].style.transformOrigin = 'top left';
+    
+  for (let i = 0; i < overlayContents.length; i++) {
+    overlayContents[i].style.transform = `scale(${scale})`;
+    overlayContents[i].style.transformOrigin = 'top left';
+  }
+
+  }
+}
+
+window.addEventListener('resize', scalePage);
+scalePage();
